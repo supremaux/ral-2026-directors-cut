e412912b583ee1dcf1ab8f5739e16b2f81bf6c5d
Suprema UX <70323230+supremaux@users.noreply.github.com>
2026-02-20 09:13:11 -0400

    correções vercel e vite

--- front/vite.config.js
+++ front/vite.config.js
@@ -1,5 +1,6 @@
 // vite.config.js
 import { defineConfig } from "vite";
+import { fileURLToPath, URL } from "node:url";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
@@ -17,5 +18,6 @@ export default defineConfig({
       },
     },
   },
+  publicDir: "src/assets", // Diretório de arquivos estáticos
   base: "./",
 });
